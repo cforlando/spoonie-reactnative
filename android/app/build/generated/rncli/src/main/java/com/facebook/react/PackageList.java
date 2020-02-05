e9eca5ef412568151e7c81f880c4ea9cfea5259f
jtcreative <jamestimberlakejr@gmail.com>
2020-02-04 23:10:45 -0500

    Added realm as a dependency

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,7 +11,11 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+import com.awesomeproject.BuildConfig;
+import com.awesomeproject.R;
 
+// realm
+import io.realm.react.RealmReactPackage;
 
 public class PackageList {
   private Application application;
@@ -56,7 +60,8 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig)
+      new MainReactPackage(mConfig),
+      new RealmReactPackage()
     ));
   }
 }
